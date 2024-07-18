f5d4ce47355b86b57740ac89c6516362607ee42b
Den <butdda@mail.ru>
2024-07-18 21:39:20 +0300

    material-react-table

--- src/components/UsersList/UserItem.jsx
+++ src/components/UsersList/UserItem.jsx
@@ -3,9 +3,11 @@ import "./UserItem.modul.css";
 const UserItem = (props) => {
     return (
         <div className="userWrapper">
-            <div className="userDataItem">{props.item.firstName}</div>
-            <div className="userDataItem">{props.item.maidenName}</div>
-            <div className="userDataItem">{props.item.lastName}</div>
+            <div className="userDataItem">
+                <div>{props.item.firstName}</div>
+                <div>{props.item.maidenName}</div>
+                <div>{props.item.lastName}</div>
+            </div>
             <div className="userDataItem">{props.item.age}</div>
             <div className="userDataItem">{props.item.gender}</div>
             <div className="userDataItem">{props.item.phone}</div>
--- src/components/UsersList/UsersList.jsx
+++ src/components/UsersList/UsersList.jsx
@@ -4,8 +4,13 @@ import {
     selectUsersList,
 } from "../../store/reducers/usersListSlice";
 import { useEffect } from "react";
-import UserItem from "./UserItem";
+// import UserItem from "./UserItem";
 import "./UsersList.modul.css";
+import { useMemo } from "react";
+import {
+    MaterialReactTable,
+    useMaterialReactTable,
+} from "material-react-table";
 
 const UsersList = () => {
     const dispatch = useDispatch();
@@ -13,18 +18,71 @@ const UsersList = () => {
         dispatch(fetchUsers());
         // eslint-disable-next-line
     }, []);
-
     const usersList = useSelector(selectUsersList);
 
-    const usersItems = usersList.map((item) => (
-        <UserItem key={item.id} item={item} />
-    ));
-    console.log(usersList);
+    const data = usersList;
+
+    // const usersItems = usersList.map((item) => (
+    //     <UserItem key={item.id} item={item} />
+    // ));
+    // console.log(usersList);
+
+    const columns = useMemo(
+        () => [
+            {
+                accessorKey: "firstName", //access nested data with dot notation
+                header: "Имя",
+                size: 50,
+            },
+            {
+                accessorKey: "maidenName", //access nested data with dot notation
+                header: "Фамилия",
+                size: 50,
+            },
+            {
+                accessorKey: "lastName", //access nested data with dot notation
+                header: "Отчество",
+                size: 50,
+            },
+            {
+                accessorKey: "age", //access nested data with dot notation
+                header: "Возраст",
+                size: 50,
+            },
+            {
+                accessorKey: "gender", //access nested data with dot notation
+                header: "Пол",
+                size: 50,
+            },
+            {
+                accessorKey: "phone", //access nested data with dot notation
+                header: "Номер телефона",
+                size: 50,
+            },
+            {
+                accessorKey: "address.city", //access nested data with dot notation
+                header: "Город",
+                size: 50,
+            },
+            {
+                accessorKey: "address.address", //access nested data with dot notation
+                header: "Адрес",
+                size: 50,
+            },
+        ],
+        []
+    );
+
+    const table = useMaterialReactTable({
+        columns,
+        data, //data must be memoized or stable (useState, useMemo, defined outside of this component, etc.)
+    });
 
     return (
         <div className="usersList">
             <h1>Users List</h1>
-            <div className="usersListWrapper">{usersItems}</div>
+            {/* <div className="usersListWrapper">{usersItems}</div> */}
+            <MaterialReactTable table={table} />
         </div>
     );
 };
